bbc41546cf3e15f915104863d8bb13869650e20a
DanielhCarranza <ingbiodanielh@gmail.com>
2022-01-18 17:44:43 -0700

    init modules

---
+++ cellsignal/models/convnext.py
@@ -0,0 +1,10 @@
+"""Adapted Implementation of ConvNext for 8-bit x 6 channel images. """
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.model.layers import trunc_normal_, DropPath
+from timm.model.registry import register_model
+
+
+
